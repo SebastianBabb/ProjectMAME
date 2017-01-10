e33da55668b682ac643b8f4a8ec3bad98154673b
Sebastian Babb <sebastianbabb@gmail.com>
2017-01-10 13:43:22 -0800

    initial commit

---
+++ web/index.php
@@ -0,0 +1,74 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+
+        <title>MAME::System Manager</title>
+        <link rel="icon" href="img/favicon.png">
+
+        <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+        <link href="css/style.css" rel="stylesheet" media="screen">
+    </head>
+    <body>
+        <div id='grid-top'></div>
+        <div id='header-container'>
+            <div class='container header-text-container'>
+                <h1>Sebastian's Arcade</h1>
+                <h2>System Manager</h2>
+            </div>
+            <hr class='header-line'>
+        </div>
+        <div class='container'>
+            <div class='container'>
+                <div class='col-md-5'>
+                    <div class='panel row'>
+                        <div class='panel-header'>
+                            System Information 
+                        </div>
+                        <div class='panel-body'>
+                            <ul>
+                                <li><span>Hostname:</span></li> 
+                                <li><span>Operating System:</span></li> 
+                                <li><span>IP:</span></li> 
+                            </ul>
+                        </div>
+                    </div>
+                    <div class='panel row'>
+                        <div class='panel-header'>
+                           Add ROM File 
+                        </div>
+                        <div class='panel-body'>
+                            <ul>
+                                <li><span>Hostname:</span></li> 
+                                <li><span>Operating System:</span></li> 
+                                <li><span>IP:</span></li> 
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class='panel col-md-offset-1 col-md-6'>
+                    <div class='panel-header'>
+                        ROMs 
+                    </div>
+                        <div class='panel-body'>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div id='grid-bottom' class="footer"></div>
+    </body>
+</html>
+
+
+<?php
+/**
+ * Short description for index.php
+ *
+ * @package index
+ * @author sebastian <sebastian@masterControl>
+ * @version 0.1
+ * @copyright (C) 2017 sebastian <sebastian@masterControl>
+ * @license MIT
+ */
